9c1cb976eb0df0129f4561acc71be00eab5929bb
BazichenkoKos <bazichenko88@gmail.com>
2016-03-25 20:02:51 +0200

    Buffer with clear. first edition
    Just Buffer with clear.

---
+++ buffer.js
@@ -0,0 +1,27 @@
+function makeCounter() {
+  var currentCount ="";
+
+function pusher(value){
+	if(value === undefined){
+  	return currentCount;
+  };
+  return currentCount +=" "+value;
+  };
+  pusher.clear = function(){
+  	return currentCount ="" ;
+  	  };   
+
+    return pusher;
+}
+
+var counter = makeCounter();
+
+
+counter("I");
+counter("made");
+console.log(counter());
+counter("all");
+counter("right");
+console.log(counter());
+counter.clear();
+console.log(counter());
